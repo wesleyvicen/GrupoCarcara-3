51ed16296d2267061e01f00787059de3be4d1464
Gabriel Carreiras Botelho <gabriel.botelho@tenti.com.br>
2021-01-31 19:27:43 -0300

    Correção de bug no dashboardservice

--- src/service/DashboardService.js
+++ src/service/DashboardService.js
@@ -7,18 +7,14 @@ const DashBoardService = {
             Authorization: Auth.getAuthToken()
         },          
     },
-    realizarLancamento: (body) => {
+    realizarLancamento: function (body) {
         return axios.post(baseURL + "lancamentos", body, this.options)            
     },
-    recuperarDadosDashboard: (params) => {
+    recuperarDadosDashboard: function (params) {
         let dataInicio = "?inicio=" + params.dataInicio,
         dataFim = "&fim=" + params.dataFim,
         login = "&login=" + params.login;
-        return axios.get(baseURL + "dashboard" + dataInicio + dataFim + login, {
-            headers:{
-                Authorization: params.token
-            }
-        })
+        return axios.get(baseURL + "dashboard" + dataInicio + dataFim + login, this.options)
     }
 }
 export default DashBoardService;
